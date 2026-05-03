5d8afac2356bdc9b79c1151898f3e85a496f68ce
sammy <getembesammy@gmail.com>
2026-05-03 18:09:10 +0300

    add searchbar.jsx

---
+++ components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SearchBar({ setSearchTerm }) {
+  return (
+    <input
+      type="text"
+      placeholder="Search projects..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className="search"
+    />
+  );
+}
+
+export default SearchBar;
