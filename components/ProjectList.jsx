44d42d4e261c4b377d6d59638f2bf17d12537b55
sammy <getembesammy@gmail.com>
2026-05-03 19:07:38 +0300

    add ProjectList

---
+++ components/ProjectList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+function ProjectList({ projects }) {
+  return (
+    <div className="grid">
+      {projects.map(project => (
+        <ProjectCard key={project.id} project={project} />
+      ))}
+    </div>
+  );
+}
+
+export default ProjectList;
